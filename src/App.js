7897308d7965049e9f01f7c7c6304fb1e08f79c5
eliran.natan <eliran.natan@axonius.com>
2022-08-20 18:27:48 +0300

    sds

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+async function loginUser(credentials) {
+  return fetch('http://localhost:8080/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+    .then(data => data.json())
+}
+
 function App() {
+
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const token = await loginUser({
+      username,
+      password
+    });
+    console.log(token)
+    //setToken(token);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +44,19 @@ function App() {
           Learn React
         </a>
       </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Username</p>
+          <input type="text" onChange={e => setUserName(e.target.value)} />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" onChange={e => setPassword(e.target.value)} />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>      
     </div>
   );
 }
