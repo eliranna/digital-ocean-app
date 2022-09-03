3d22bda21bacef8ba604321ef4512a78ce4c703d
eliran-natan <eliran.natan@axonius.com>
2022-09-03 03:41:40 +0300

    sdsd

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -103,7 +103,7 @@ const SelectBox = styled.select`
 async function sendRegistrationForm(formData) {
     return axios({
         method: 'post',
-        url: 'http://localhost:8080/open-registration-request',
+        url: '/open-registration-request',
         data: formData,
         headers: {
             'Content-Type': 'multipart/form-data'
